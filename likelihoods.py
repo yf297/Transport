e9e51b8fee1f6b4d409a699d330772fdd4884147
yf297 <yf297@cornell.edu>
2025-01-12 19:13:26 -0500

    typos

--- likelihoods.py
+++ likelihoods.py
@@ -3,7 +3,7 @@ import generate
 
 def ll(y, mean, cov):
     mvn_conditional = MultivariateNormal(mean, cov)
-    log_prob = mvn_conditional.log_prob(y).sum()
+    log_prob = mvn_conditional.log_prob(y)
     return -1*log_prob
 
 
@@ -14,13 +14,13 @@ def full_ll(y, x, model):
     likelihood = 0
     
     for i in range(1, n+1):
-                       
+        t0x = generate.space_time( t[i-1].unsqueeze(0), x )               
         y0 = y[(i-1),:]
-        y1 = y[i,:]
-        t0x = generate.space_time( t[i-1].unsqueeze(0), x )
-        t1x = generate.space_time( t[i].unsqueeze(0), x )
-        
         model.set_train_data(t0x, y0, strict=False)
+        
+        t1x = generate.space_time( t[i].unsqueeze(0), x )
+        y1 = y[i,:]
+        
         mean = model.likelihood(model(t1x)).mean
         cov = model.likelihood(model(t1x)).covariance_matrix
         likelihood += ll(y1, mean, cov)
