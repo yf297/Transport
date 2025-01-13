7d834630a4c1920d991a0a809ead2ac95b62e6a8
yf297 <yf297@cornell.edu>
2025-01-12 19:14:36 -0500

    more width

--- net.py
+++ net.py
@@ -12,7 +12,7 @@ def initialize(model):
     nn.init.constant_(model.output_bias,0)
 
 class Flow(nn.Module):
-    def __init__(self, L=1, h=32, d=2):
+    def __init__(self, L=1, h=48, d=2):
         super(Flow, self).__init__()
                 
         self.hidden_weights = nn.ParameterList()
@@ -20,12 +20,14 @@ class Flow(nn.Module):
         
         weight = nn.Parameter(torch.Tensor(h, d))
         self.hidden_weights.append(weight)
+        
         bias = nn.Parameter(torch.Tensor(h))
         self.hidden_biases.append(bias)
         
         for _ in range(L-1):
             weight = nn.Parameter(torch.Tensor(h, h))
             self.hidden_weights.append(weight)
+            
             bias = nn.Parameter(torch.Tensor(h))
             self.hidden_biases.append(bias)
         
@@ -39,20 +41,20 @@ class Flow(nn.Module):
         
         weight = self.hidden_weights[0]
         bias = self.hidden_biases[0]
-        out = F.linear(out, weight) + t * bias 
-        out = F.gelu(out)
+        out = F.gelu(F.linear(out, weight) + t * bias)
 
         for weight, bias in zip(self.hidden_weights[1:], self.hidden_biases[1:]):
-            out = F.linear(out, weight) + bias
-            out = F.gelu(out)
+            out = F.gelu(F.linear(out, weight) + bias)
 
-        out = F.linear(out, self.output_weight) + self.output_bias
+        weight = self.output_weight
+        bias = self.output_bias
+        out = F.linear(out,weight) + bias
 
         return x + t * out
 
 
 class Vel(nn.Module):
-    def __init__(self, L=1, h=32, d=2):
+    def __init__(self, L=1, h=48, d=2):
         super(Vel, self).__init__()
                 
         self.hidden_weights = nn.ParameterList()
@@ -60,12 +62,14 @@ class Vel(nn.Module):
         
         weight = nn.Parameter(torch.Tensor(h, d))
         self.hidden_weights.append(weight)
+        
         bias = nn.Parameter(torch.Tensor(h))
         self.hidden_biases.append(bias)
         
         for _ in range(L-1):
             weight = nn.Parameter(torch.Tensor(h, h))
             self.hidden_weights.append(weight)
+            
             bias = nn.Parameter(torch.Tensor(h))
             self.hidden_biases.append(bias)
         
@@ -82,20 +86,20 @@ class Vel(nn.Module):
         weight = self.hidden_weights[0]
         bias = self.hidden_biases[0]
         
-        out = F.linear(out, weight) + t * bias
-        out = F.gelu(out)
+        out = F.gelu(F.linear(out, weight) + t * bias)
 
         for weight, bias in zip(self.hidden_weights[1:], self.hidden_biases[1:]):
-            out = F.linear(out, weight) + bias
-            out = F.gelu(out)
+            out = F.gelu(F.linear(out, weight) + bias)
 
-        out = F.linear(out, self.output_weight) + self.output_bias
+        weight = self.output_weight
+        bias = self.output_bias
+        out = F.linear(out,weight) + bias
         return out
 
 
 
 class Mean(nn.Module):
-    def __init__(self, L=1, h=32, d=2):
+    def __init__(self, L=1, h=48, d=2):
         super(Mean, self).__init__()
                 
         self.hidden_weights = nn.ParameterList()
@@ -103,12 +107,14 @@ class Mean(nn.Module):
         
         weight = nn.Parameter(torch.Tensor(h, d))
         self.hidden_weights.append(weight)
+        
         bias = nn.Parameter(torch.Tensor(h))
         self.hidden_biases.append(bias)
         
         for _ in range(L-1):
             weight = nn.Parameter(torch.Tensor(h, h))
             self.hidden_weights.append(weight)
+            
             bias = nn.Parameter(torch.Tensor(h))
             self.hidden_biases.append(bias)
         
@@ -117,17 +123,16 @@ class Mean(nn.Module):
         
         
     def forward(self, x):
-        
         out = x
         
         weight = self.hidden_weights[0]
         bias = self.hidden_biases[0]
-        out = F.linear(out, weight) + bias
-        out = F.gelu(out)
+        out = F.gelu(F.linear(out, weight) + bias)
 
         for weight, bias in zip(self.hidden_weights[1:], self.hidden_biases[1:]):
-            out = F.linear(out, weight) 
-            out = F.gelu(out)
+            out = F.gelu(F.linear(out, weight) + bias)
 
-        out = F.linear(out, self.output_weight) + self.output_bias
+        weight = self.output_weight
+        bias = self.output_bias
+        out = F.linear(out,weight) + bias
         return out
