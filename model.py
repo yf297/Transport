816477776441ab8610cbc269767ec1fe23006a4d
yf297 <yf297@cornell.edu>
2025-01-12 20:40:30 -0500

    cosmetics

--- model.py
+++ model.py
@@ -6,35 +6,31 @@ import net
 class Mean(gpytorch.means.Mean):
     def __init__(self, **kwargs):
         super(Mean, self).__init__(**kwargs)
-        self.Mean_W = net.Mean(L = 4)
-        net.initialize(self.Mean_W)
+        self.func = net.Scalar_Field(L = 4)
+        net.initialize(self.func)
         
     def forward(self, a, **params):
-        m = torch.vmap(self.Mean_W)(a).squeeze()
+        m = torch.vmap(self.func)(a).squeeze()
         return m
 
-class Kernel(gpytorch.kernels.Kernel):
-    def __init__(self, **kwargs):
-        super(Kernel, self).__init__(**kwargs)
-        self.Kernel_W = gpytorch.kernels.ScaleKernel(
-                        gpytorch.kernels.MaternKernel(
-                        nu = 3/2, ard_num_dims = 2))
-    
-    def forward(self, a1, a2, **params):
-        return self.Kernel_W(a1, a2)
-    
+
 class GP(gpytorch.models.ExactGP):
     def __init__(self, tx, y, Flow, Vel, likelihood):
         super(GP, self).__init__(tx, y, likelihood)
         self.Flow = Flow
         self.Vel = Vel
-        self.Mean = Mean()
-        self.Kernel = Kernel()
-
+        self.Mean_Initial = Mean()
+        self.Kernel_Initial = gpytorch.kernels.ScaleKernel(
+                              gpytorch.kernels.MaternKernel(
+                                nu = 3/2, 
+                                ard_num_dims = 2))
+        
     def forward(self, tx):
         a = torch.vmap(self.Flow)(tx)
-        mean = self.Mean(a)
-        covar = self.Kernel(a, a)
+        
+        mean = self.Mean_Initial(a)
+        covar = self.Kernel_Initial(a, a)
+       
         return gpytorch.distributions.MultivariateNormal(mean, covar)
 
 def Initialize(Flow, Vel):
