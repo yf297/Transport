42c44f572c3e0822a70ae12c73bfe34e203ba256
yf297 <yf297@cornell.edu>
2025-01-12 19:14:00 -0500

    change where flow is defined

--- model.py
+++ model.py
@@ -4,55 +4,46 @@ import gpytorch
 import net
 
 class Mean(gpytorch.means.Mean):
-    def __init__(self, Flow, **kwargs):
+    def __init__(self, **kwargs):
         super(Mean, self).__init__(**kwargs)
-        self.Flow = Flow
         self.Mean_W = net.Mean(L = 4)
         net.initialize(self.Mean_W)
         
-    def forward(self, tx, **params):
-        a = torch.vmap(self.Flow)(tx)
+    def forward(self, a, **params):
         m = torch.vmap(self.Mean_W)(a).squeeze()
         return m
 
 class Kernel(gpytorch.kernels.Kernel):
-    def __init__(self, Flow, **kwargs):
+    def __init__(self, **kwargs):
         super(Kernel, self).__init__(**kwargs)
-        self.Flow = Flow
         self.Kernel_W = gpytorch.kernels.ScaleKernel(
                         gpytorch.kernels.MaternKernel(
                         nu = 3/2, ard_num_dims = 2))
     
-    def forward(self, tx1, tx2, **params):
-
-        a1 = torch.vmap(self.Flow)(tx1)
-        a2 = torch.vmap(self.Flow)(tx2)
-
-        return self.Kernel_W(a1, a2) 
+    def forward(self, a1, a2, **params):
+        return self.Kernel_W(a1, a2)
     
 class GP(gpytorch.models.ExactGP):
     def __init__(self, tx, y, Flow, Vel, likelihood):
         super(GP, self).__init__(tx, y, likelihood)
         self.Flow = Flow
         self.Vel = Vel
-        self.Mean = Mean(self.Flow)
-        self.Kernel = Kernel(self.Flow)
+        self.Mean = Mean()
+        self.Kernel = Kernel()
 
-    def forward(self, x):
-        mean = self.Mean(x)
-        covar = self.Kernel(x, x)
+    def forward(self, tx):
+        a = torch.vmap(self.Flow)(tx)
+        mean = self.Mean(a)
+        covar = self.Kernel(a, a)
         return gpytorch.distributions.MultivariateNormal(mean, covar)
 
-class Initialize:
-    def __init__(self, Flow, Vel):
-        self.Flow = Flow
-        net.initialize(self.Flow)
-        
-        self.Vel = Vel
-        net.initialize(self.Vel)
-        
-        likelihood = gpytorch.likelihoods.GaussianLikelihood()
-        tx = None
-        y = None
-        
-        self.GP = GP(tx, y, Flow, Vel, likelihood)
+def Initialize(Flow, Vel):
+    
+    net.initialize(Flow)
+    net.initialize(Vel)
+    
+    likelihood = gpytorch.likelihoods.GaussianLikelihood()
+    tx = None
+    y = None
+    
+    return GP(tx, y, Flow, Vel, likelihood)
