f48b2fa724e0f6975a134b1c6a65b7628c02a52a
yf297 <yf297@cornell.edu>
2025-01-21 16:35:29 -0500

    mish

--- net.py
+++ net.py
@@ -1,138 +1,51 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-
-
+import torch
 def initialize(model):
-    for weight, bias in zip(model.hidden_weights, model.hidden_biases):
-        nn.init.kaiming_uniform_(weight, nonlinearity="relu")
-        nn.init.constant_(bias,0)
-    
-    nn.init.kaiming_uniform_(model.output_weight, nonlinearity="relu")
-    nn.init.constant_(model.output_bias,0)
+    for module in model.modules():
+        if isinstance(module, nn.Linear):
+            nn.init.xavier_uniform_(module.weight, gain=nn.init.calculate_gain('relu'))
+            if module.bias is not None:
+                nn.init.constant_(module.bias, 0)
+
 
 class Flow(nn.Module):
-    def __init__(self, L=1, h=48, d=2):
+    def __init__(self, d=2, L=1, h=32):
         super(Flow, self).__init__()
-                
-        self.hidden_weights = nn.ParameterList()
-        self.hidden_biases = nn.ParameterList()
+    
+        layers = [nn.Linear(d + 1, h), nn.Mish()]
+        for _ in range(L - 1):
+            layers += [nn.Linear(h, h), nn.Mish()]
+        layers.append(nn.Linear(h, d))
         
-        weight = nn.Parameter(torch.Tensor(h, d))
-        self.hidden_weights.append(weight)
+        self.network = nn.Sequential(*layers)
         
-        bias = nn.Parameter(torch.Tensor(h))
-        self.hidden_biases.append(bias)
+    def forward(self, xyt):
+        xy = xyt[0:2]
+        t = xyt[2:]
+        return xy + t*self.network(xyt) 
+    
+class Vel(nn.Module):
+    def __init__(self, d=2, L=1, h=32):
+        super(Vel, self).__init__()
         
-        for _ in range(L-1):
-            weight = nn.Parameter(torch.Tensor(h, h))
-            self.hidden_weights.append(weight)
-            
-            bias = nn.Parameter(torch.Tensor(h))
-            self.hidden_biases.append(bias)
+        layers = [nn.Linear(d + 1, h), nn.Mish()]
+        for _ in range(L - 1):
+            layers += [nn.Linear(h, h), nn.Mish()]
+        layers.append(nn.Linear(h, d))
         
-        self.output_weight = nn.Parameter(torch.Tensor(d, h))
-        self.output_bias = nn.Parameter(torch.Tensor(d))
+        self.network = nn.Sequential(*layers)
         
-    def forward(self, tx):
-        t = tx[0:1]
-        x = tx[1:]
-        out = x
-        
-        weight = self.hidden_weights[0]
-        bias = self.hidden_biases[0]
-        out = F.gelu(F.linear(out, weight) + t * bias)
+    def forward(self, xyt):
+        return self.network(xyt)
 
-        for weight, bias in zip(self.hidden_weights[1:], self.hidden_biases[1:]):
-            out = F.gelu(F.linear(out, weight) + bias)
-
-        weight = self.output_weight
-        bias = self.output_bias
-        out = F.linear(out,weight) + bias
-
-        return x + t * out
-
-
-class Vector_Field(nn.Module):
-    def __init__(self, L=1, h=48, d=2):
-        super(Vector_Field, self).__init__()
-                
-        self.hidden_weights = nn.ParameterList()
-        self.hidden_biases = nn.ParameterList()
+class Mean(nn.Module):
+    def __init__(self, d=2, L=1, h=32):
+        super(Mean, self).__init__()
+        layers = [nn.Linear(d, h) , nn.Mish()]   
+        for _ in range(L - 1):
+            layers += [nn.Linear(h, h) , nn.Mish()]
+        layers.append( nn.Linear(h, 1) )
+        self.network = nn.Sequential(*layers)
         
-        weight = nn.Parameter(torch.Tensor(h, d))
-        self.hidden_weights.append(weight)
-        
-        bias = nn.Parameter(torch.Tensor(h))
-        self.hidden_biases.append(bias)
-        
-        for _ in range(L-1):
-            weight = nn.Parameter(torch.Tensor(h, h))
-            self.hidden_weights.append(weight)
-            
-            bias = nn.Parameter(torch.Tensor(h))
-            self.hidden_biases.append(bias)
-        
-        self.output_weight = nn.Parameter(torch.Tensor(d, h))
-        self.output_bias = nn.Parameter(torch.Tensor(d))
-        
-        
-    def forward(self, tx):
-        
-        t = tx[0:1]
-        x = tx[1:]
-        out = x
-        
-        weight = self.hidden_weights[0]
-        bias = self.hidden_biases[0]
-        
-        out = F.gelu(F.linear(out, weight) + t * bias)
-
-        for weight, bias in zip(self.hidden_weights[1:], self.hidden_biases[1:]):
-            out = F.gelu(F.linear(out, weight) + bias)
-
-        weight = self.output_weight
-        bias = self.output_bias
-        out = F.linear(out,weight) + bias
-        return out
-
-
-
-class Scalar_Field(nn.Module):
-    def __init__(self, L=1, h=48, d=2):
-        super(Scalar_Field, self).__init__()
-                
-        self.hidden_weights = nn.ParameterList()
-        self.hidden_biases = nn.ParameterList()
-        
-        weight = nn.Parameter(torch.Tensor(h, d))
-        self.hidden_weights.append(weight)
-        
-        bias = nn.Parameter(torch.Tensor(h))
-        self.hidden_biases.append(bias)
-        
-        for _ in range(L-1):
-            weight = nn.Parameter(torch.Tensor(h, h))
-            self.hidden_weights.append(weight)
-            
-            bias = nn.Parameter(torch.Tensor(h))
-            self.hidden_biases.append(bias)
-        
-        self.output_weight = nn.Parameter(torch.Tensor(1, h))
-        self.output_bias = nn.Parameter(torch.Tensor(1))
-        
-        
-    def forward(self, x):
-        out = x
-        
-        weight = self.hidden_weights[0]
-        bias = self.hidden_biases[0]
-        out = F.gelu(F.linear(out, weight) + bias)
-
-        for weight, bias in zip(self.hidden_weights[1:], self.hidden_biases[1:]):
-            out = F.gelu(F.linear(out, weight) + bias)
-
-        weight = self.output_weight
-        bias = self.output_bias
-        out = F.linear(out,weight) + bias
-        return out
+    def forward(self, a):
+        return self.network(a)
