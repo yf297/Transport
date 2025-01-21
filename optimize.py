d440399b184019d04674a91c7c0fce289012e9e1
yf297 <yf297@cornell.edu>
2025-01-21 16:34:33 -0500

    sep optimizers

--- optimize.py
+++ optimize.py
@@ -1,83 +1,60 @@
 import torch
-from likelihoods import ll, full_ll
-import generate
 import penalty
+import gpytorch
 
-def initial_state(y, x, model, num_epochs=200):
+def flow(spacetime, z, model, num_epochs=75):
     
-    n = y.shape[0]-1
-    t = generate.time(n)
+    model.gp.set_train_data(spacetime, z, strict=False)
     
-    optimizer = torch.optim.Adam([
-        {'params': model.Mean.parameters()},
-        {'params': model.Kernel.parameters(), 'lr': 0.1},
-        {'params': model.likelihood.parameters(), 'lr': 0.1}
+    
+    optimizer_net = torch.optim.Adam([
+        {'params': model.flow.parameters(),'lr': 0.01},
+        {'params': model.gp.Mean.Mean_W.parameters(),'lr': 0.01}])
+   
+    optimizer_cov = torch.optim.Adam([            
+        {'params': model.gp.Kernel.Kernel_W.parameters(), 'lr': 0.1},
+        {'params': model.gp.likelihood.parameters(), 'lr': 0.1}
     ])
+
+    scheduler_net = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer_net, 
+        mode='min', 
+        factor=0.1, 
+        patience=2, 
+        min_lr=1e-5
+    )
     
-    y0 = y[0, :]
-    t0x = generate.space_time( t[0].unsqueeze(0), x )
-    model.set_train_data(t0x, y0, strict=False)
+    scheduler_cov = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer_net, 
+        mode='min', 
+        factor=0.1, 
+        patience=2, 
+        min_lr=1e-5
+    )
+
     
-    m = y0.numel()
-    model.train()
-    
-    for epoch in range(1,num_epochs+1):
+    mll = gpytorch.mlls.ExactMarginalLogLikelihood(model.gp.likelihood, model.gp)
+
+    for epoch in range(1, num_epochs+1):
+        optimizer_net.zero_grad()
+        optimizer_cov.zero_grad()
         
-        optimizer.zero_grad() 
-        
-        mean = model.likelihood(model(t0x)).mean
-        cov = model.likelihood(model(t0x)).covariance_matrix 
-        likelihood = ll(y0, mean, cov)
-        
-        loss = likelihood/m
+        output = model.gp(spacetime)
+        ll = -mll(output, z) 
+                       
+        loss = ll
         loss.backward()
-        optimizer.step()
-            
-        if epoch % 50 == 0:
-            print(f'Epoch: {epoch:d} - Likelihood: {likelihood.item()/m:.3f}')
 
+        optimizer_net.step()
+        optimizer_cov.step()
+        
+        scheduler_net.step(ll)
+        scheduler_cov.step(ll)
+        
+        current_lrs = [round(group['lr'], 6) for group in optimizer_net.param_groups]
 
-
-def flow(y, x, model, num_epochs=20):
-
-    n = y.shape[0]-1   
-    N = y.numel()
-    m = x.shape[0]
-    
-    t = generate.time(n)
-    tx = generate.space_time(t,x)
-
-    optimizer = torch.optim.Adam([
-        {'params': model.Flow.parameters()},
-        {'params': model.Vel.parameters()},
-        {'params': model.likelihood.parameters(), 'lr': 0.1}
-    ])
-    
-
-    model.eval()
-    for epoch in range(1,num_epochs+1):
-        for i in range(1,n+1):
-            optimizer.zero_grad()
-
-            t0x = generate.space_time( t[i-1].unsqueeze(0), x )
-            y0 = y[(i-1),:]
-            model.set_train_data(t0x, y0, strict=False)
-            
-            t1x = generate.space_time( t[i].unsqueeze(0), x )            
-            y1 = y[i,:]
-            
-            mean = model.likelihood(model(t1x)).mean
-            cov = model.likelihood(model(t1x)).covariance_matrix
-            likelihood = ll(y1, mean, cov)
-            
-            pen = penalty.PDE(tx, model.Flow, model.Vel)
-            alpha = 0.01 * torch.abs((likelihood / m) / pen).item()
-            
-            loss = likelihood/m + alpha*pen
-            loss.backward()
-            optimizer.step()
-            
-                
         if epoch % 5 == 0:
-            full_likelihood = full_ll(y, x, model)
-            print(f'Epoch: {epoch:.2f} - Likelihood: {full_likelihood.item()/N:.3f}')
+            print(f"Epoch: {epoch} - Likelihood: {ll.item():.3f} - Learning Rates: {current_lrs}")
+
+        if 1e-5 in current_lrs:
+            break
