c0aba8ab3746eae1178a40e5bf82b2fef6bde950
yf297 <yf297@cornell.edu>
2025-01-21 16:35:03 -0500

    smoothness 1/2

--- model.py
+++ model.py
@@ -1,45 +1,58 @@
-
 import torch
 import gpytorch
 import net
 
 class Mean(gpytorch.means.Mean):
-    def __init__(self, **kwargs):
+    def __init__(self, flow, **kwargs):
         super(Mean, self).__init__(**kwargs)
-        self.func = net.Scalar_Field(L = 4)
-        net.initialize(self.func)
+        self.flow = flow
+        self.Mean_W = net.Mean(L = 3)
+        net.initialize(self.Mean_W)
         
-    def forward(self, a, **params):
-        m = torch.vmap(self.func)(a).squeeze()
+    def forward(self, xyt, **params):
+        a = torch.vmap(self.flow)(xyt)
+        m = torch.vmap(self.Mean_W)(a).squeeze()
         return m
 
-
+class Kernel(gpytorch.kernels.Kernel):
+    def __init__(self, flow, **kwargs):
+        super(Kernel, self).__init__(**kwargs)
+        self.flow = flow
+        self.Kernel_W = gpytorch.kernels.ScaleKernel(
+                        gpytorch.kernels.MaternKernel(
+                        nu = 1/2, ard_num_dims = 2))
+    
+    def forward(self, xyt1, xyt2, **params):
+        t1 = xyt1[:,2:]
+        t2 = xyt2[:,2:]
+        
+        a1 = torch.vmap(self.flow)(xyt1)
+        a2 = torch.vmap(self.flow)(xyt2)
+        
+        #ta1 = torch.cat([t1,a1], dim = 1)
+        #ta2 = torch.cat([t2,a2], dim = 1)
+        
+        return self.Kernel_W(a1, a2)
+    
 class GP(gpytorch.models.ExactGP):
-    def __init__(self, tx, y, Flow, Vel, likelihood):
-        super(GP, self).__init__(tx, y, likelihood)
-        self.Flow = Flow
-        self.Vel = Vel
-        self.Mean_Initial = Mean()
-        self.Kernel_Initial = gpytorch.kernels.ScaleKernel(
-                              gpytorch.kernels.MaternKernel(
-                                nu = 3/2, 
-                                ard_num_dims = 2))
+    def __init__(self, xyt, z, flow, likelihood):
+        super(GP, self).__init__(xyt, z, likelihood)
+        self.Mean = Mean(flow)
+        self.Kernel = Kernel(flow)
         
-    def forward(self, tx):
-        a = torch.vmap(self.Flow)(tx)
-        
-        mean = self.Mean_Initial(a)
-        covar = self.Kernel_Initial(a, a)
-       
+    def forward(self, xyt):
+        mean = self.Mean(xyt)
+        covar = self.Kernel(xyt, xyt)
         return gpytorch.distributions.MultivariateNormal(mean, covar)
 
-def Initialize(Flow, Vel):
-    
-    net.initialize(Flow)
-    net.initialize(Vel)
-    
-    likelihood = gpytorch.likelihoods.GaussianLikelihood()
-    tx = None
-    y = None
-    
-    return GP(tx, y, Flow, Vel, likelihood)
+class GP_FLOW:
+    def __init__(self, flow, vel):
+        self.flow = flow
+        net.initialize(flow)
+
+        self.vel = vel
+        self.likelihood = gpytorch.likelihoods.GaussianLikelihood()
+        xyt = None
+        z = None
+        
+        self.gp = GP(xyt, z, flow, self.likelihood)
