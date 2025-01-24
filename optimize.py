1e861d44695bbaca6c487a45c93204c016b287ae
yf297 <yf297@cornell.edu>
2025-01-24 16:06:48 -0500

    remove set train

--- optimize.py
+++ optimize.py
@@ -3,24 +3,24 @@ import penalty
 import gpytorch
 
 def flow(spacetime, z, model, num_epochs=75):
-    
-    model.gp.set_train_data(spacetime, z, strict=False)
-    
-    
+        
     optimizer_net = torch.optim.Adam([
-        {'params': model.flow.parameters(),'lr': 0.01},
-        {'params': model.gp.Mean.Mean_W.parameters(),'lr': 0.01}])
+        {'params': model.flow.parameters(),'lr': 0.001},
+        {'params': model.gp.Mean.parameters(),'lr': 0.001}])
+    
+    optimizer_vel = torch.optim.AdamW([
+       {'params': model.vel.parameters(),'lr': 0.01, "weight_decay": 0.01}])
    
     optimizer_cov = torch.optim.Adam([            
-        {'params': model.gp.Kernel.Kernel_W.parameters(), 'lr': 0.1},
+        {'params': model.gp.Kernel.parameters(), 'lr': 0.1},
         {'params': model.gp.likelihood.parameters(), 'lr': 0.1}
     ])
-
+    
     scheduler_net = torch.optim.lr_scheduler.ReduceLROnPlateau(
         optimizer_net, 
         mode='min', 
         factor=0.1, 
-        patience=2, 
+        patience=1, 
         min_lr=1e-5
     )
     
@@ -28,25 +28,27 @@ def flow(spacetime, z, model, num_epochs=75):
         optimizer_net, 
         mode='min', 
         factor=0.1, 
-        patience=2, 
+        patience=1, 
         min_lr=1e-5
     )
 
-    
     mll = gpytorch.mlls.ExactMarginalLogLikelihood(model.gp.likelihood, model.gp)
 
     for epoch in range(1, num_epochs+1):
+        
         optimizer_net.zero_grad()
         optimizer_cov.zero_grad()
+        optimizer_vel.zero_grad()
         
         output = model.gp(spacetime)
         ll = -mll(output, z) 
                        
-        loss = ll
+        loss = ll + 0.01*penalty.PDE(spacetime, model.flow, model.vel)
         loss.backward()
 
         optimizer_net.step()
         optimizer_cov.step()
+        optimizer_vel.step()
         
         scheduler_net.step(ll)
         scheduler_cov.step(ll)
