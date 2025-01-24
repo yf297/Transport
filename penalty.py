ddcc163d0b1bbf99cc22baaf21c1d51d00a5046e
yf297 <yf297@cornell.edu>
2025-01-24 16:04:19 -0500

    spacetime

--- penalty.py
+++ penalty.py
@@ -1,18 +1,29 @@
 import torch
 
-def D_phi(tx, flow):
-    func = lambda tx: flow(tx)
-    return torch.func.jacrev(func)(tx)
+def D_phi(xyt, flow):
+    func = lambda xyt: flow(xyt)
+    return torch.func.jacrev(func)(xyt)
 
-def PDE(tx, flow, vel):
+def v_hat(xyt, flow):
+    Jacobians = D_phi(xyt, flow)
+    Jacobians_t = Jacobians[:, 2]
+    Jacobians_x = Jacobians[:, 0:2]
 
-    loss = torch.nn.L1Loss()
-    v =  torch.vmap(vel)(tx)
+    v = torch.linalg.solve(-1*Jacobians_x, Jacobians_t)
+    return v
+
+
+def PDE(spacetime, flow, vel):
+
+    loss = torch.nn.L1Loss(reduction="mean")
     
-    D =  torch.vmap(D_phi, in_dims=(0, None))(tx, flow)
-    Dt = D[:, :, 0]
-    Dx = D[:, :, 1:]
+    v =  torch.vmap(vel)(spacetime)
+    D =  torch.vmap(D_phi, in_dims=(0, None))(spacetime, flow)
+    
+    Dt = D[:, :, 2]
+    Dx = D[:, :, 0:2]
     rhs = torch.matmul(Dx,v.unsqueeze(-1)).squeeze(2)
     lhs = Dt
     
     return loss(lhs, -1*rhs) 
+
