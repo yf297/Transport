050d79504fff5c64dc42e43b20a71460ff03cd91
yf97 <yf297@cornell.edu>
2025-01-29 10:10:05 -0500

    back to og

--- net.py
+++ net.py
@@ -1,44 +1,21 @@
 import torch.nn as nn
 
-
-class ResidualBlock(nn.Module):
-    def __init__(self, h):
-        super().__init__()
-        self.linear1 = nn.Linear(h, h)
-        self.ln1 = nn.LayerNorm(h)
-        self.activation = nn.Mish()
-        self.linear2 = nn.Linear(h, h)
-        self.ln2 = nn.LayerNorm(h)
-        self._initialize_weights()
-
-    def _initialize_weights(self):
-        nn.init.kaiming_normal_(self.linear1.weight,  a=0.0003)
-        nn.init.kaiming_normal_(self.linear2.weight,  a=0.0003)
-        if self.linear1.bias is not None:
-            nn.init.zeros_(self.linear1.bias)
-        if self.linear2.bias is not None:
-            nn.init.zeros_(self.linear2.bias)
-
-    def forward(self, x):
-        residual = x
-        x = self.linear1(x)
-        x = self.ln1(x)
-        x = self.activation(x)
-        x = self.linear2(x)
-        x = self.ln2(x)
-        return self.activation(x + residual)
-
-                
 class Flow(nn.Module):
     def __init__(self, d=2, L=1, h=32):
         super(Flow, self).__init__()
         
-        layers = [nn.Linear(d + 1, h), nn.Mish()]
+        layers = [nn.Linear(d+1, h) , nn.GELU()]   
         for _ in range(L - 1):
-            layers.append(ResidualBlock(h))
-        layers.append(nn.Linear(h, d))
-        
+            layers += [nn.Linear(h, h) , nn.GELU()]
+        layers.append( nn.Linear(h, d) )
         self.network = nn.Sequential(*layers)
+        
+        for module in self.modules():
+            if isinstance(module, nn.Linear):
+                nn.init.kaiming_uniform_(module.weight, nonlinearity="relu")
+                if module.bias is not None:
+                    nn.init.constant_(module.bias, 0)
+                    
                 
     def forward(self, txy):
         xy = txy[...,1:]
@@ -49,17 +26,19 @@ class Flow(nn.Module):
 class Mean(nn.Module):
     def __init__(self, d=2, L=1, h=32):
         super(Mean, self).__init__()
-        
-        layers = [nn.Linear(d, h), nn.Mish()]
+        layers = [nn.Linear(d, h) , nn.GELU()]   
         for _ in range(L - 1):
-            layers.append(ResidualBlock(h))
-        layers.append(nn.Linear(h, 1))
-        
+            layers += [nn.Linear(h, h) , nn.GELU()]
+        layers.append( nn.Linear(h, 1) )
         self.network = nn.Sequential(*layers)
-                
+        
+        for module in self.modules():
+            if isinstance(module, nn.Linear):
+                nn.init.kaiming_uniform_(module.weight, nonlinearity="relu")
+                if module.bias is not None:
+                    nn.init.constant_(module.bias, 0)
+                    
     def forward(self, a):
         return self.network(a).squeeze()
-    
-
 
 
