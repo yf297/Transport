b5a201d2b99eb9e9cd448e761a150c34b66f5775
yf97 <yf297@cornell.edu>
2025-02-02 10:57:04 -0500

    adamw

--- optimize.py
+++ optimize.py
@@ -1,98 +1,59 @@
 import torch
-import penalty
 import gpytorch
-import gc 
+from likelihoods import ll
+import gc
+import penalty
 
-def flow(model, num_epochs=75):
+import torch
+import gpytorch
+import gc
+
+def initial(model, num_epochs=75):
     
-    if torch.cuda.is_available():
-        model.TXY = model.TXY.cuda()
-        model.Z = model.Z.cuda()
+    optimizer = torch.optim.AdamW([
+        {'params': model.gp.Mean.parameters()},
+        {'params': model.flow.parameters()},
+        {'params': model.vel.parameters()},
+        {'params': model.gp.Kernel.parameters(), 'lr': 0.1, 'weight_decay':0},
+        {'params': model.gp.likelihood.parameters(),'lr': 0.1, 'weight_decay':0},
+    ])
+    
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_epochs)
+    
+    if torch.cuda.is_available():   
         model.gp = model.gp.cuda()
         model.likelihood = model.likelihood.cuda()
-
-    optimizer_mean = torch.optim.Adam([
-        {'params': model.gp.Mean.parameters(), 'lr': 0.001}
-    ])
-    
-    optimizer_flow = torch.optim.Adam([
-        {'params': model.flow.parameters(), 'lr': 0.001}
-    ])
-   
-    optimizer_cov = torch.optim.Adam([
-        {'params': model.gp.Kernel.parameters(), 'lr': 0.1},
-        {'params': model.gp.likelihood.parameters(), 'lr': 0.1}
-    ])
-    
-    scheduler_mean = torch.optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer_mean, 
-        mode='min', 
-        factor=0.1, 
-        patience=1, 
-        min_lr=1e-5
-    )
-    
-    scheduler_flow = torch.optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer_flow, 
-        mode='min', 
-        factor=0.1, 
-        patience=1, 
-        min_lr=1e-5
-    )
-    
-    scheduler_cov = torch.optim.lr_scheduler.ReduceLROnPlateau(
-        optimizer_cov, 
-        mode='min', 
-        factor=0.1, 
-        patience=1, 
-        min_lr=1e-5
-    )
-
+        model.flow = model.flow.cuda()
+        model.vel = model.vel.cuda()
+        model.TXY = model.TXY.cuda()
+        model.Z = model.Z.cuda()
+        
     mll = gpytorch.mlls.ExactMarginalLogLikelihood(model.likelihood, model.gp)
-
-
-    for epoch in range(1, num_epochs+1):
-        
-        optimizer_mean.zero_grad()
-        optimizer_cov.zero_grad()
-        optimizer_flow.zero_grad()
-        
+    model.gp.train()
+    
+    for epoch in range(1, num_epochs + 1):
+        optimizer.zero_grad()
         output = model.gp(model.TXY)
-        ll = -mll(output, model.Z) 
-                        
-        loss = ll
+        ll = -mll(output, model.Z)
+        
+        loss = ll + 0.001 * penalty.PDE(model.TXY, model.flow, model.vel)
         loss.backward()
-
-        optimizer_mean.step()
-        optimizer_cov.step()
-        optimizer_flow.step()
+        optimizer.step()
         
-        scheduler_mean.step(ll)
-        scheduler_flow.step(ll)
-        scheduler_cov.step(ll)
+        #scheduler.step()
         
-        current_lrs = [round(group['lr'], 6) for group in optimizer_flow.param_groups]
-
-        if epoch % 5 == 0:
-            print(f"Epoch: {epoch} - Likelihood: {ll.item():.3f} - Learning Rates: {current_lrs}")
-
-        if 1e-5 in current_lrs:
-            if torch.cuda.is_available():
-                model.TXY = model.TXY.cpu()
-                model.Z = model.Z.cpu()
-                model.gp = model.gp.cpu()
-                model.likelihood = model.likelihood.cpu()
-                gc.collect()
-                torch.cuda.empty_cache()
-            break
-        
+        if epoch % 1 == 0:
+            print(f'Epoch: {epoch} - Likelihood: {loss.item():.2f}')
+    
     if torch.cuda.is_available():   
-        model.TXY = model.TXY.cpu()
-        model.Z = model.Z.cpu()
         model.gp = model.gp.cpu()
         model.likelihood = model.likelihood.cpu()
+        model.flow = model.flow.cpu()
+        model.vel = model.vel.cpu()
+        model.TXY = model.TXY.cpu()
+        model.Z = model.Z.cpu()
         gc.collect()
         torch.cuda.empty_cache()
 
 
-        
+
